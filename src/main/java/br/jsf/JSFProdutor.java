6746cc3890ce5591aa2ee352f095ddf47e70ec70
João Pedro Padoan <54078732+joao-padoan@users.noreply.github.com>
2024-12-09 15:54:50 -0300

    Add files via upload

---
+++ src/main/java/br/jsf/JSFProdutor.java
@@ -0,0 +1,41 @@
+package br.jsf; 
+import jakarta.annotation.Resource; 
+import jakarta.inject.Named; 
+import jakarta.enterprise.context.RequestScoped; 
+import jakarta.jms.ConnectionFactory; 
+import jakarta.jms.JMSConnectionFactory; 
+import jakarta.jms.JMSContext; 
+import jakarta.jms.Queue; 
+/** 
+* 
+* @author lucio 
+*/ 
+@Named(value = "JSFProdutor") 
+@RequestScoped 
+public class JSFProdutor { 
+    @Resource(lookup="java:comp/DefaultJMSConnectionFactory") 
+    private ConnectionFactory connectionFactory; 
+    @Resource(lookup="java/Fila") 
+    private Queue fila; 
+    /** 
+    * Creates a new instance of JSFProdutor 
+    */ 
+    private String mensagem;
+    public String getMensagem(){
+        return mensagem;
+    }
+    
+    public void setMensagem(String mensagem){
+        this.mensagem = mensagem;
+    }
+    
+    public JSFProdutor() { 
+    } 
+    public void send(){ 
+        try { 
+        JMSContext context = connectionFactory.createContext(); context.createProducer().send(fila, mensagem); 
+        } catch (Exception e){
+            System.out.println("ERRO"); System.out.println(e.getMessage()); 
+        } 
+    } 
+} 
